768b47eb7b975f9c97017af549d44eb75fc349d3
muhawenimanajoy <77844024+muhawenimanajoy@users.noreply.github.com>
2021-01-25 05:45:57 +0200

    Add files via upload

---
+++ control_statement.java
@@ -0,0 +1,14 @@
+class ForExample
+{
+	public static void main(String[] args) 
+	{
+		//int i;
+		int i=0;
+		while(i<=10)
+		{
+			System.out.println(i+".I study in IPRC Tumba");
+
+		}
+		i++;
+	}
+}
